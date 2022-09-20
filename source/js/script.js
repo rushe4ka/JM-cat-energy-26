ce9cb2fe53f3ad29958710a13854fdfc85c5494f
Julija Markova <julia.markova.ru@gmail.com>
2022-09-20 18:05:32 +0200

    Делает меню открытым для версии без JS

--- source/js/script.js
+++ source/js/script.js
@@ -4,11 +4,11 @@ let navToggle = document.querySelector('.main-nav__toggle');
 navMain.classList.remove('main-nav--nojs');
 
 navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
-  } else {
-    navMain.classList.add('main-nav--closed');
+  if (navMain.classList.contains('main-nav--opened')) {
     navMain.classList.remove('main-nav--opened');
+    navMain.classList.add('main-nav--closed');
+  } else {
+    navMain.classList.add('main-nav--opened');
+    navMain.classList.remove('main-nav--closed');
   }
 });
