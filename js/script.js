b8ae06ab3f18e30982a822b4ee9b555f099e676e
rushe4ka <32070997+rushe4ka@users.noreply.github.com>
2022-10-02 12:53:01 +0000

    Deploying to gh-pages from @ rushe4ka/JM-cat-energy-26@115982fd15b4673574486374afc211454212fd0d 🚀

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let navMain = document.querySelector('.main-nav');
+let navToggle = document.querySelector('.main-nav__toggle');
+
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('main-nav--opened')) {
+    navMain.classList.remove('main-nav--opened');
+    navMain.classList.add('main-nav--closed');
+  } else {
+    navMain.classList.add('main-nav--opened');
+    navMain.classList.remove('main-nav--closed');
+  }
+});
